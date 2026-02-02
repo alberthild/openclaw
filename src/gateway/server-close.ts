4d1b3eaf26aca7df88dcb5ced26c20e37e7b2f54
Claudia <claudia.keller0001@gmail.com>
2026-02-02 21:10:58 +0100

    style: Run oxfmt on all changed files

--- src/gateway/server-close.ts
+++ src/gateway/server-close.ts
@@ -125,7 +125,7 @@ export function createGatewayCloseHandler(params: {
         httpServer.close((err) => (err ? reject(err) : resolve())),
       );
     }
-    
+
     // Shutdown Event Store connection
     await shutdownEventStore().catch(() => {});
   };
